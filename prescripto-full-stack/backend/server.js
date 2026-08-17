1dd581feb445b0b40ae63dd24ed6ceaf1ca4bfb1
Sanjay5734 <sanjay57342246@gmail.com>
2026-08-17 21:48:11 +0530

    add

--- prescripto-full-stack/backend/server.js
+++ prescripto-full-stack/backend/server.js
@@ -1,29 +1,41 @@
-import express from "express"
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from "./config/mongodb.js"
-import connectCloudinary from "./config/cloudinary.js"
-import userRouter from "./routes/userRoute.js"
-import doctorRouter from "./routes/doctorRoute.js"
-import adminRouter from "./routes/adminRoute.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import adminRouter from "./routes/adminRoute.js";
 
-// app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
 
-// middlewares
-app.use(express.json())
-app.use(cors())
+// Connect to database and Cloudinary
+connectDB();
+connectCloudinary();
 
-// api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/admin", adminRouter)
-app.use("/api/doctor", doctorRouter)
+// Middlewares
+app.use(express.json());
 
+app.use(
+  cors({
+    origin: "https://prescripto-2-oaxz.onrender.com",
+    credentials: true,
+  })
+);
+
+// API endpoints
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+
+// Test API
 app.get("/", (req, res) => {
-  res.send("API Working")
+  res.send("API Working");
 });
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
+// Start server
+app.listen(port, () => {
+  console.log(`Server started on PORT:${port}`);
+});
